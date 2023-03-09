aece8a9f38a11764c85e74701cff9436547249e8
Konstantin Ivlev <ivlkostya@yandex.ru>
2023-03-09 22:15:58 +1000

    second wershion

--- Figures. Sides and corners.cpp
+++ Figures. Sides and corners.cpp
@@ -6,32 +6,69 @@ class Figure
 {
 protected:
 	std::string nameOfFigure;
-	int haveSides = 3;
-	int sideLengthA= 0;
-	int sideLengthB = 0;
-	int sideLengthC = 0;
-	int sideLengthD = 0;
-	int angleA = 0;
-	int angleB = 0;
-	int angleC = 0;
-	int angleD = 0;
+	int haveSides = 0;
+//	int sideLengthA= 0;
+//	int sideLengthB = 0;
+//	int sideLengthC = 0;
+//	int sideLengthD = 0;
+//	int angleA = 0;
+//	int angleB = 0;
+//	int angleC = 0;
+//	int angleD = 0;
 public:
 	std::string getNameOfFigure() { return nameOfFigure; }
+	int getSides() { return haveSides; }
+//
+//	int getSideLengthA() { return sideLengthA; }
+//	int getSideLengthB() { return sideLengthB; }
+//	int getSideLengthC() { return sideLengthC; }
+//	int getSideLengthD() { return sideLengthD; }
+//
+//	int getAngleA() { return angleA; }
+//	int getAngleB() { return angleB; }
+//	int getAngleC() { return angleC; }
+//	int getAngleD() { return angleD; }
+	virtual void printFigure()
+		
+	{
+		std::cout << std::endl;
+		std::cout << getNameOfFigure() << ":" << std::endl;
+
+		/*std::cout << "Стороны:";
+		
+			
+		
+		std::cout << "Углы:";*/
+			}
+};
+class Triangle : public Figure
+{
+protected:
+	
+		int haveSides = 3;
+		int sideLengthA= 0;
+		int sideLengthB = 0;
+		int sideLengthC = 0;
+	//	int sideLengthD = 0;
+		int angleA = 0;
+		int angleB = 0;
+		int angleC = 0;
+	//	int angleD = 0;
+
+
+public:
 	int getSides() { return haveSides; }
 
 	int getSideLengthA() { return sideLengthA; }
 	int getSideLengthB() { return sideLengthB; }
 	int getSideLengthC() { return sideLengthC; }
-	int getSideLengthD() { return sideLengthD; }
-
+//	int getSideLengthD() { return sideLengthD; }
+//
 	int getAngleA() { return angleA; }
 	int getAngleB() { return angleB; }
 	int getAngleC() { return angleC; }
-	int getAngleD() { return angleD; }
-};
-class Triangle : public Figure
-{
-public:
+//	int getAngleD() { return angleD; }
+
 	Triangle(int sideLengthA, int sideLengthB, int sideLengthC, int angleA, int angleB, int angleC)
 	{
 		nameOfFigure = "Треугольник";
@@ -41,6 +78,21 @@ public:
 		this->angleA = angleA;
 		this->angleB = angleB;
 		this->angleC = angleC;
+	}
+	void printFigure()override
+	{
+		std::cout << std::endl;
+		std::cout << getNameOfFigure() << ":" << std::endl;
+
+		std::cout << "Стороны:";
+		std::cout << " a = " << getSideLengthA() << ", b = " << getSideLengthB() << ", с = " << getSideLengthC();
+		
+	std::cout << std::endl;
+	
+
+		std::cout << "Углы:";
+		std::cout << " А = " << getAngleA() << ", В = " << getAngleB() << ", С = " << getAngleC();
+		
 	}
 };
 
@@ -73,6 +125,49 @@ public:
 };
 class Quadrilateral : public Figure
 {
+
+protected:
+
+	int haveSides = 4;
+	int sideLengthA = 0;
+	int sideLengthB = 0;
+	int sideLengthC = 0;
+	int sideLengthD = 0;
+	int angleA = 0;
+	int angleB = 0;
+	int angleC = 0;
+	int angleD = 0;
+public:
+
+	int getSides() { return haveSides; }
+
+	int getSideLengthA() { return sideLengthA; }
+	int getSideLengthB() { return sideLengthB; }
+	int getSideLengthC() { return sideLengthC; }
+	int getSideLengthD() { return sideLengthD; }
+
+	int getAngleA() { return angleA; }
+	int getAngleB() { return angleB; }
+	int getAngleC() { return angleC; }
+	int getAngleD() { return angleD; }
+
+
+	void printFigure() override
+	{
+		std::cout << std::endl;
+		std::cout << getNameOfFigure() << ":" << std::endl;
+
+		std::cout << "Стороны:";
+		std::cout << " a = " << getSideLengthA() << ", b = " << getSideLengthB() << ", с = " << getSideLengthC();
+
+		std::cout << ", d = " << getSideLengthD() << std::endl;
+
+		std::cout << "Углы:";
+		std::cout << " А = " << getAngleA() << ", В = " << getAngleB() << ", С = " << getAngleC();
+		std::cout << ", D = " << getAngleD() << std::endl;
+	}
+
+
 
 public:
 	Quadrilateral(int sideLengthA, int sideLengthB, int sideLengthC, int sideLengthD, int angleA, int angleB, int angleC, int angleD)
@@ -129,24 +224,24 @@ public:
 		Quadrilateral::nameOfFigure = "Ромб";
 	}
 };
-void printFigure(Figure& figure)
-{
-	std::cout << std::endl;
-	std::cout << figure.getNameOfFigure() << ":" << std::endl;
-
-	std::cout << "Стороны:";
-	std::cout << " a = " << figure.getSideLengthA() << ", b = " << figure.getSideLengthB() << ", с = " << figure.getSideLengthC();
-	if (figure.getSides()==3)
-	{
-		std::cout << ", d = " << figure.getSideLengthD() << std::endl;
-	}
-	else { std::cout << std::endl; }
-
-	std::cout << "Углы:";
-	std::cout << " А = " << figure.getAngleA() << ", В = " << figure.getAngleB() << ", С = " << figure.getAngleC();
-	if (figure.getSides()==4) { std::cout << ", D = " << figure.getAngleD() << std::endl; }
-	else { std::cout << std::endl; }
-}
+//void printFigure(Figure& figure)
+//{
+//	std::cout << std::endl;
+//	std::cout << figure.getNameOfFigure() << ":" << std::endl;
+//
+//	std::cout << "Стороны:";
+//	std::cout << " a = " << figure.getSideLengthA() << ", b = " << figure.getSideLengthB() << ", с = " << figure.getSideLengthC();
+//	if (figure.getSides()==3)
+//	{
+//		std::cout << ", d = " << figure.getSideLengthD() << std::endl;
+//	}
+//	else { std::cout << std::endl; }
+//
+//	std::cout << "Углы:";
+//	std::cout << " А = " << figure.getAngleA() << ", В = " << figure.getAngleB() << ", С = " << figure.getAngleC();
+//	if (figure.getSides()==4) { std::cout << ", D = " << figure.getAngleD() << std::endl; }
+//	else { std::cout << std::endl; }
+//}
 
 
 int main()
@@ -155,40 +250,29 @@ int main()
 	system("chcp 1251");
 
 	Triangle Triangle1(10, 20, 30, 50,60, 60);
-	printFigure(Triangle1);
+	Triangle1.printFigure();
 
 	rightTriangle rightTriangle1(10, 20, 30, 40, 50);
-	printFigure(rightTriangle1);
+	rightTriangle1.printFigure();
 
 	isoscelesTriangle isoscelesTriangle1(10, 50, 60, 50);
-	printFigure(isoscelesTriangle1);
+	isoscelesTriangle1.printFigure();
 
 	equilateralTriangle equilateralTriangle1(30);
-	printFigure(equilateralTriangle1);
+	equilateralTriangle1.printFigure();
 
 	Quadrilateral Quadrilateral1(10, 20, 30, 40, 50, 60, 70, 80);
-	printFigure(Quadrilateral1);
+	Quadrilateral1.printFigure();
 
 	Rectangle Rectangle1(10, 20);
-	printFigure(Rectangle1);
+	Rectangle1.printFigure();
 
 	Square Square1(30);
-	printFigure(Square1);
+	Square1.printFigure();
 
 	Parallelogram Parallelogram1(20, 30, 30, 40);
-	printFigure(Parallelogram1);
+	Parallelogram1.printFigure();
 
 	Rhomb Rhomb1(30, 30, 40);
-	printFigure(Rhomb1);
+	Rhomb1.printFigure();
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
